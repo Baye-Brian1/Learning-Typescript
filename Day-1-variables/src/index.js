d68353a39d1ff99245add1efd889ccd16706043d
Baye-Brian1 <bayebrian89@gmail.com>
2026-08-01 14:54:18 +0100

    Understanding Arrays in Typescript

--- Day-1-variables/src/index.js
+++ Day-1-variables/src/index.js
@@ -1,5 +1,15 @@
 "use strict";
+//I. Variables and Types
 Object.defineProperty(exports, "__esModule", { value: true });
+//A) Types
+// 1-string
+// 2-number
+// 3-boolean: either true or false
+// 4-null
+// 5-unidentified
+// 6-symbol: here it goes for unique variables
+// 7-any: it is rarely used because it cancels the verifications of SVGUnitTypes ETC.
+//B) Variables
 let fullName = "Baye Brian";
 let age = 20;
 let Occupation = "Student";
@@ -29,4 +39,17 @@ console.log(`Ordered Qty: ${Quantity}`);
 console.log(`Initial Available Qty: ${inStock}`);
 console.log(`Total: ${totalPrice}`);
 console.log(`Final Available Qty: ${QuantityAvailable}`);
+//II. Arrays and Objects
+// A) Arrays
+let fruits = ["Apple", "Banana", "Mango", "Grapes"];
+console.log(fruits[3]);
+fruits.push("Orange");
+console.log(fruits);
+let scores = [20, 40, 60, 80, 100];
+console.log(scores);
+let completed = [true, false, true, false];
+console.log(completed);
+console.log(completed[0]);
+let Sentence = ["Baye Brian", 20, "Frontend Developer", "currently Learning", "Typescript"];
+console.log(Sentence);
 //# sourceMappingURL=index.js.map
--- Day-1-variables/src/index.ts
+++ Day-1-variables/src/index.ts
@@ -1,3 +1,15 @@
+//I. Variables and Types
+
+//A) Types
+// 1-string
+// 2-number
+// 3-boolean: either true or false
+// 4-null
+// 5-unidentified
+// 6-symbol: here it goes for unique variables
+// 7-any: it is rarely used because it cancels the verifications of SVGUnitTypes ETC.
+
+//B) Variables
 let fullName: string="Baye Brian";
 let age: number = 20;
 let Occupation: string= "Student";
@@ -31,6 +43,25 @@ console.log(`Ordered Qty: ${Quantity}`);
 console.log(`Initial Available Qty: ${inStock}`);
 console.log(`Total: ${totalPrice}`);
 console.log(`Final Available Qty: ${QuantityAvailable}`);
+
+//II. Arrays and Objects
+
+// A) Arrays
+let fruits: string[] =["Apple", "Banana", "Mango", "Grapes"];
+console.log(fruits[3]);
+fruits.push("Orange");
+console.log(fruits);
+
+let scores: number[]= [20, 40, 60, 80, 100];
+console.log(scores);
+
+let completed: boolean[]=[true, false, true, false];
+console.log(completed);
+console.log(completed[0]);
+
+let Sentence: (string | number)[]= ["Baye Brian", 20, "Frontend Developer", "currently Learning", "Typescript"]
+console.log(Sentence);
+
 
 
 
