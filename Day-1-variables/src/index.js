e814c6c6f2014f4123624aec354ab0efa7433c1e
Baye-Brian1 <bayebrian89@gmail.com>
2026-08-01 14:07:04 +0100

    Day-1 understanding Types and Variables

---
+++ Day-1-variables/src/index.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let fullName = "Baye Brian";
+let age = 20;
+let Occupation = "Student";
+let School = "COLTECH";
+let Department = "Computer Engineering";
+let Level = 400;
+let isFrontEndDeveloper = true;
+let FavouriteLanguage = "English Language";
+console.log(`Name: ${fullName}`);
+console.log(`Age: ${age}`);
+console.log(`School: ${School}`);
+console.log(`Occupation ${Occupation}`);
+console.log(`Department ${Department}`);
+console.log(`Level: ${Level}`);
+console.log(`Language: ${FavouriteLanguage}`);
+console.log(`Frontend Developer: ${isFrontEndDeveloper}`);
+//# sourceMappingURL=index.js.map
---
+++ Day-1-variables/src/index.ts
@@ -0,0 +1,20 @@
+let fullName: string="Baye Brian";
+let age: number = 20;
+let Occupation: string= "Student";
+let School: string ="COLTECH"
+let Department: string="Computer Engineering"
+let Level: number= 400;
+let isFrontEndDeveloper: boolean = true
+let FavouriteLanguage: string= "English Language"
+
+console.log(`Name: ${fullName}`);
+console.log(`Age: ${age}`);
+console.log(`School: ${School}`);
+console.log(`Occupation: ${Occupation}`);
+console.log(`Department: ${Department}`);
+console.log(`Level: ${Level}`);
+console.log(`Language: ${FavouriteLanguage}`);
+console.log(`Frontend Developer: ${isFrontEndDeveloper}`);
+
+
+
