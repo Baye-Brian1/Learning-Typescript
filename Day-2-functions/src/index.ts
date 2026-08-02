f44b46b9a990556fb0c6447d0f8ff8771f7e2ff1
Baye-Brian1 <bayebrian89@gmail.com>
2026-08-02 14:27:40 +0100

    worked with parameter types functions and arrays together

--- Day-2-functions/src/index.ts
+++ Day-2-functions/src/index.ts
@@ -32,56 +32,123 @@
 // Salut("Brian", "UK")
 
 
-console.log("=====Full Name and Age of a person=====");
-const fullName=(name:string, age:number)=>{
-    console.log(`my name is ${name} and im ${age} years old`);
-}
-fullName("Baye Brian", 20);
+// console.log("=====Full Name and Age of a person=====");
+// const fullName=(name:string, age:number)=>{
+//     console.log(`my name is ${name} and im ${age} years old`);
+// }
+// fullName("Baye Brian", 20);
 
 
-console.log("=====SQUARE OF A NUMBER=====");
-const Square=(value:number):number=>{
-    return value*value;
-}
+// console.log("=====SQUARE OF A NUMBER=====");
+// const Square=(value:number):number=>{
+//     return value*value;
+// }
 
-console.log(Square(5));
+// console.log(Square(5));
 
 
-console.log("=====Succses Verification=====");
-const Verify=(input:number)=>{
-    if(input>=50){
-        console.log("Student succesfully passed");
-    }else{
-        console.log("Student Failed");
-    }
-}
+// console.log("=====Succses Verification=====");
+// const Verify=(input:number)=>{
+//     if(input>=50){
+//         console.log("Student succesfully passed");
+//     }else{
+//         console.log("Student Failed");
+//     }
+// }
 
-Verify(70);
-Verify(40);    
-// OR
-console.log("=====Success or Score Verification=====");
-const Check=(score:number):boolean=>{
+// Verify(70);
+// Verify(40);    
+// // OR
+// console.log("=====Success or Score Verification=====");
+// const Check=(score:number):boolean=>{
 
- if(score <= 49) {
-    return false;
- }  
- else{
-    return true;
- }
-};
- const Passed= Check(50)
- console.log(Passed?"Student Passed":"Student Failed");
+//  if(score <= 49) {
+//     return false;
+//  }  
+//  else{
+//     return true;
+//  }
+// };
+//  const Passed= Check(50)
+//  console.log(Passed?"Student Passed":"Student Failed");
  
 
-console.log("=====Address Verification=====");
-const address=(name:string, phone?:number): void=>{
-    console.log(`Name: ${name}`);
-    if(phone){
-        console.log(`Phone: ${phone}`);   
-    }else{
-        console.log("phone: Unprovided");
+// console.log("=====Address Verification=====");
+// const address=(name:string, phone?:number): void=>{
+//     console.log(`Name: ${name}`);
+//     if(phone){
+//         console.log(`Phone: ${phone}`);   
+//     }else{
+//         console.log("phone: Unprovided");
+//     }
+    
+// }
+// address("Brian");
+// address("Brian", 675717944);
+
+console.log("=====Student Grade Calculator======");
+ type Students={
+    name:string;
+    course: string;
+    scores: number[];
+    average: number;
+    grade: string;
+ }
+const students: Students[]=[
+];
+
+const calculateAverage=(scores:number[])=>{ 
+    const total= scores.reduce((num, score) =>{
+        return num+score;
+    },0)
+    const Average= total/scores.length;
+    return Average;
+}
+const getGrade=(Average:number)=>{
+    if (Average>=80){
+        return "A";   
+    }else if (Average>=70){
+        return "B";   
+    }
+    else if (Average>=60){
+        return "C";   
+    }
+    else if (Average>=50){
+        return "D";   
+    }
+    else{
+        return "F";   
+    }
+}
+const displayStudent=()=>{
+ students.forEach(student =>{
+    console.log(student.name, student.course, student.scores, student.average, student.grade);
+ })   
+}
+const scores=[30, 60, 90, 60]
+const average= calculateAverage(scores);
+
+const grades= getGrade(average)
+
+const addStudent=()=>{
+    
+    const nestaStud={
+        name: "Nesta",
+        course: "Management",
+        scores: scores,
+        average: average,
+        grade: grades
+    }
+    const brianStud=  {
+        name: "Brian",
+        course: "Software En",
+        scores: scores,
+        average: average,
+        grade: grades
     }
     
+   students.push(brianStud);
+   students.push(nestaStud);
 }
-address("Brian");
-address("Brian", 675717944);
+addStudent()
+displayStudent()
