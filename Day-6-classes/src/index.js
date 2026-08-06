28efcd78c708567afffe9c761842adf0038de05e
Baye-Brian1 <bayebrian89@gmail.com>
2026-08-06 23:33:45 +0100

    almost done

--- Day-6-classes/src/index.js
+++ Day-6-classes/src/index.js
@@ -48,4 +48,55 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // }
 // const s = new Students("Joebass")
 // s.display()
+class Student {
+    id;
+    name;
+    course;
+    scores;
+    average = 0;
+    constructor(id, name, course, scores) {
+        this.id = id;
+        this.name = name;
+        this.course = course;
+        this.scores = scores;
+        this.calculateAverage();
+    }
+    calculateAverage() {
+        if (this.scores.length === 0) {
+            this.average = 0;
+            return;
+        }
+        const total = this.scores.reduce((sum, score) => sum + score, 0);
+        this.average = total / this.scores.length;
+    }
+    displayStudent() {
+        console.log(`ID: ${this.id} | Name: Mr/Mrs. ${this.name} | Course: ${this.course}
+      | Scores ${this.scores} | Average: ${this.average}`);
+    }
+}
+class StudentManager {
+    students = [];
+    addStudent(student) {
+        this.students.push(student);
+        console.log(`Added: ${student.name}`);
+    }
+    removeStudent(id) {
+        const before = this.students.length;
+        this.students = this.students.filter(student => student.id !== id);
+        if (this.students.length < before) {
+            console.log(`Removed student with ID: ${id}`);
+        }
+        else {
+            console.log(`Student with ID: ${id} not found`);
+        }
+    }
+    displayStudents() {
+        console.log(`Students`);
+        if (this.students.length === 0) {
+            console.log("No students");
+            return;
+        }
+        this.students.forEach(student => student.displayStudent());
+    }
+}
 //# sourceMappingURL=index.js.map
--- Day-6-classes/src/index.ts
+++ Day-6-classes/src/index.ts
@@ -9,7 +9,7 @@
 
 //   display(){
 //     console.log(`${this.name} is ${this.age} years old`);
-    
+
 //   }
 // }
 
@@ -53,9 +53,67 @@
 // class Students extends Person{
 //   display(){
 //      console.log(this.name);
-     
+
 //   }
 // }
 
 // const s = new Students("Joebass")
 // s.display()
+
+class Student {
+  id: number;
+  name: string;
+  course: string;
+  scores: number[];
+  average: number = 0;
+  constructor(id: number, name: string, course: string, scores: number[]) {
+    this.id = id;
+    this.name = name;
+    this.course = course;
+    this.scores = scores;
+    this.calculateAverage();
+  }
+  calculateAverage() {
+    if (this.scores.length === 0) {
+      this.average = 0;
+      return;
+    }
+    const total = this.scores.reduce((sum, score) => sum + score, 0);
+    this.average = total / this.scores.length;
+  }
+  displayStudent() {
+    console.log(`ID: ${this.id} | Name: Mr/Mrs. ${this.name} | Course: ${this.course}
+      | Scores ${this.scores} | Average: ${this.average}`);
+  }
+}
+
+class StudentManager{
+  private students: Student[]=[];
+
+  addStudent(student: Student){
+    this.students.push(student)
+    console.log(`Added: ${student.name}`);
+    
+  }
+  removeStudent(id:number){
+    const before = this.students.length;
+    this.students = this.students.filter(student =>student.id !== id);
+    if (this.students.length < before){
+      console.log(`Removed student with ID: ${id}`);
+    }else{
+      console.log(`Student with ID: ${id} not found`);
+      
+    }
+  }
+
+  displayStudents(){
+    console.log(`Students`);
+    if(this.students.length ===0){
+      console.log("No students");
+      return
+      
+    }
+    this.students.forEach(student => student.displayStudent())  
+  }
+}
+
