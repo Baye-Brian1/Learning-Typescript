c6a0cbd1a33408b575663b84afd7a275f2006f49
Baye-Brian1 <bayebrian89@gmail.com>
2026-08-13 20:10:05 +0100

    Advance API calls

--- Day-8-API/src/index.js
+++ Day-8-API/src/index.js
@@ -1,21 +1,20 @@
+"use strict";
 // interface Student{
 //   id: number;
 //   name: string;
 //   course: string;
 // }
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // const student: Student={
 //   id: 1,
 //   name: "Brian",
 //   course: "software"
 // }
-
 // // async function fetchStudents():Promise<Student[]> {
 // //   const response= await fetch("https://example.com/students");
 // //   const students: Student[]=await response.json();
 // //   return students
 // // }
-
 // async function fetchStudents():Promise<Student[]> {
 //   try {
 //     const response= await fetch("https://example.com/students")
@@ -29,13 +28,11 @@
 //     throw error;
 //   }
 // }
-
 // interface Product{
 //   id: number;
 //   title: string;
 //   price: number;
 // }
-
 // const products: Product[]=[
 //   {
 //     id: 1,
@@ -62,14 +59,11 @@
 //     title: "Couch",
 //     price: 1000
 //   },
-
 // ];
-
 // interface ApiResponse<T>{
 //   success: boolean;
 //   data: T;
 // }
-
 // async function getProducts(): Promise<Product[]>{
 //   const response: ApiResponse<Product[]>={
 //     success: true,
@@ -87,13 +81,10 @@
 //     ]
 //   }
 //   await new Promise(resolve =>setTimeout(resolve, 1000))
-
 //   const Pro: Product[]= await response.data;
 //   return Pro;
 // }
-
 // // getProducts().then(products => console.log(products));
-
 // async function fetchUsers(){
 //   try {
 //      const response= await fetch("https://fakestoreapi.com/products")  
@@ -105,9 +96,8 @@
 //   } catch (error) {
 //     console.log("Error:", error);
 //     throw error
-    
 //   }
 // }
 // console.log("===Users===");
 // fetchUsers
-
+//# sourceMappingURL=index.js.map
