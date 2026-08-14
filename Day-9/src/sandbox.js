cda3b985b495203af6c84346b00540355c60d56c
Baye-Brian1 <bayebrian89@gmail.com>
2026-08-14 15:37:01 +0100

    Dom Manipulation

--- Day-9/src/sandbox.js
+++ Day-9/src/sandbox.js
@@ -1,61 +1,41 @@
-class Invoice {
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes £${this.amount} for ${this.details}`;
-    }
-}
-class Payment {
-    constructor(recipient, details, amount) {
-        this.recipient = recipient;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.recipient} is owed £${this.amount} for ${this.details}`;
-    }
-}
-class ListTemplate {
-    constructor(container) {
-        this.container = container;
-    }
-    render(item, heading, position) {
-        const li = document.createElement('li');
-        const h4 = document.createElement('h4');
-        h4.innerText = heading;
-        li.append(h4);
-        const p = document.createElement('p');
-        p.innerText = item.format();
-        li.append(p);
-        if (position === 'start') {
-            this.container.prepend(li);
-        }
-        else {
-            this.container.append(li);
-        }
-    }
-}
-const form = document.querySelector('.new-item-form');
-console.log(form.children);
-const type = document.querySelector('#type');
-const toFrom = document.querySelector('#toFrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-const ul = document.querySelector('ul');
-const list = new ListTemplate(ul);
+// interface HasFormatter{
+//     format():string
+//  }
+//  class Invoice implements HasFormatter{
+//     constructor(
+//         readonly client: string,
+//         private details: string,
+//         public amount: number
+//     ){}
+const form = document.querySelector('#transactionForm');
+const type = document.querySelector('#transactionType');
+const toFrom = document.querySelector('#transactionSource');
+const details = document.querySelector('#transactionDetails');
+const amount = document.querySelector('#transactionAmount');
+const transactionList = document.querySelector('#transactionList');
+const Income = {
+    id: 1,
+    type: "income",
+    source: toFrom.value,
+    details: details.value,
+    amount: amount.valueAsNumber,
+    date: "1"
+};
+console.log(Income);
+const transaction = {
+    id: 1,
+    type: "income",
+    source: "Salary",
+    details: "Monthly salary",
+    amount: 500000,
+    date: "2026-08-14"
+};
+console.log(transaction);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    let doc;
-    if (type.value === 'invoice') {
-        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    if (type.value === "income") {
+        console.log(toFrom.value, details.value, amount.valueAsNumber);
     }
-    else {
-        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
-    }
-    list.render(doc, type.value, "start");
 });
 export {};
 //# sourceMappingURL=sandbox.js.map
--- Day-9/src/sandbox.ts
+++ Day-9/src/sandbox.ts
@@ -1,68 +1,119 @@
-interface HasFormatter{
-    format():string
- }
- class Invoice implements HasFormatter{
-    constructor(
-        readonly client: string,
-        private details: string,
-        public amount: number
-    ){}
+// interface HasFormatter{
+//     format():string
+//  }
+//  class Invoice implements HasFormatter{
+//     constructor(
+//         readonly client: string,
+//         private details: string,
+//         public amount: number
+//     ){}
 
-    format(){
-        return `${this.client} owes £${this.amount} for ${this.details}`
-    }
-}
- class Payment implements HasFormatter{
-    constructor(
-        readonly recipient: string,
-        private details: string,
-        public amount: number
-    ){}
+//     format(){
+//         return `${this.client} owes £${this.amount} for ${this.details}`
+//     }
+// }
+//  class Payment implements HasFormatter{
+//     constructor(
+//         readonly recipient: string,
+//         private details: string,
+//         public amount: number
+//     ){}
 
-    format(){
-        return `${this.recipient} is owed £${this.amount} for ${this.details}`
-    }
-}
-class ListTemplate{
-    constructor(private container: HTMLUListElement){}
-    render(item: HasFormatter, heading:string, position:'start'|'end'){
-        const li= document.createElement('li')
+//     format(){
+//         return `${this.recipient} is owed £${this.amount} for ${this.details}`
+//     }
+// }
+// class ListTemplate{
+//     constructor(private container: HTMLUListElement){}
+//     render(item: HasFormatter, heading:string, position:'start'|'end'){
+//         const li= document.createElement('li')
         
-        const h4= document.createElement('h4')
-        h4.innerText= heading;
-        li.append(h4);
+//         const h4= document.createElement('h4')
+//         h4.innerText= heading;
+//         li.append(h4);
 
-        const p= document.createElement('p');
-        p.innerText= item.format()
-        li.append(p)
+//         const p= document.createElement('p');
+//         p.innerText= item.format()
+//         li.append(p)
 
-        if (position=== 'start'){
-            this.container.prepend(li)
-        }else{
-            this.container.append(li)
-        }
-    }
+//         if (position=== 'start'){
+//             this.container.prepend(li)
+//         }else{
+//             this.container.append(li)
+//         }
+//     }
+// }
+
+// const form= document.querySelector('.new-item-form') as HTMLFormElement;
+// console.log(form.children);
+
+// const type= document.querySelector('#type') as HTMLSelectElement;
+// const toFrom= document.querySelector('#toFrom') as HTMLInputElement;
+// const details= document.querySelector('#details') as HTMLInputElement;
+// const amount= document.querySelector('#amount') as HTMLInputElement;
+
+// const ul= document.querySelector('ul')!;
+// const list= new ListTemplate(ul)
+
+// form.addEventListener('submit', (e: Event)=>{
+//     e.preventDefault();
+//     let doc:HasFormatter;
+//     if(type.value==='invoice'){
+//         doc=new Invoice(toFrom.value, details.value, amount.valueAsNumber)
+//     }else{
+//         doc= new Payment(toFrom.value, details.value, amount.valueAsNumber)
+//     }
+//    list.render(doc, type.value, "start")
+    
+// });
+
+interface Transaction{
+    id: number;
+    type: "income"|"expense"
+    source: string;
+    details: string;
+    amount: number;
+    date: string;
 }
 
-const form= document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
+const form= document.querySelector('#transactionForm') as HTMLFormElement
+const type= document.querySelector('#transactionType') as HTMLSelectElement;
+const toFrom= document.querySelector('#transactionSource') as HTMLInputElement;
+const details= document.querySelector('#transactionDetails') as HTMLInputElement;
+const amount= document.querySelector('#transactionAmount') as HTMLInputElement;
 
-const type= document.querySelector('#type') as HTMLSelectElement;
-const toFrom= document.querySelector('#toFrom') as HTMLInputElement;
-const details= document.querySelector('#details') as HTMLInputElement;
-const amount= document.querySelector('#amount') as HTMLInputElement;
+const transactionList=document.querySelector('#transactionList')
 
-const ul= document.querySelector('ul')!;
-const list= new ListTemplate(ul)
+const Income:Transaction={
+    id: 1,
+    type: "income",
+    source: toFrom.value,
+    details: details.value,
+    amount: amount.valueAsNumber,
+    date: "1"
 
-form.addEventListener('submit', (e: Event)=>{
-    e.preventDefault();
-    let doc:HasFormatter;
-    if(type.value==='invoice'){
-        doc=new Invoice(toFrom.value, details.value, amount.valueAsNumber)
-    }else{
-        doc= new Payment(toFrom.value, details.value, amount.valueAsNumber)
-    }
-   list.render(doc, type.value, "start")
+}
+console.log(Income);
+
+const transaction: Transaction = {
+    id: 1,
+
+    type: "income",
+
+    source: "Salary",
+
+    details: "Monthly salary",
+
+    amount: 500000,
+
+    date: "2026-08-14"
+};
+console.log(transaction);
+
+form.addEventListener('submit', (e:Event)=>{
+ e.preventDefault();
+ if (type.value==="income"){
+    console.log(toFrom.value, details.value, amount.valueAsNumber);
     
-});
+ }
+})
