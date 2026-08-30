b95c9ae8f1d8c133b8b9586dcdf50cc948ed8949
Baye-Brian1 <bayebrian89@gmail.com>
2026-08-30 16:37:20 +0100

    made it interactive

--- Day-10-dashboard/src/sandbox.js
+++ Day-10-dashboard/src/sandbox.js
@@ -1,11 +1,106 @@
-const popup = document.querySelector('.add-modal-card');
-const openModal = document.querySelector('#addPopup');
-const closeModal = document.querySelector('#closePopup');
-openModal === null || openModal === void 0 ? void 0 : openModal.addEventListener('click', () => {
-    popup === null || popup === void 0 ? void 0 : popup.classList.add('show');
+const popup = document.querySelector(".add-modal-card");
+const openModal = document.querySelector("#addPopup");
+const closeModal = document.querySelector("#closePopup");
+openModal.addEventListener("click", () => {
+    popup === null || popup === void 0 ? void 0 : popup.classList.add("show");
 });
-closeModal === null || closeModal === void 0 ? void 0 : closeModal.addEventListener('click', () => {
-    popup === null || popup === void 0 ? void 0 : popup.classList.remove('show');
+closeModal.addEventListener("click", () => {
+    popup === null || popup === void 0 ? void 0 : popup.classList.remove("show");
 });
+const productName = document.querySelector("#productName");
+const productCategory = document.querySelector("#productCategory");
+const productAmount = document.querySelector("#productAmount");
+const productQuantity = document.querySelector("#productQuantity");
+const productList = document.querySelector("#productList");
+const submitBtn = document.querySelector("#addProductButton");
+const form = document.querySelector("#productForm");
+const products = [];
+const STORAGE = "products";
+const saveProduct = () => {
+    try {
+        const data = JSON.stringify(products);
+        localStorage.setItem(STORAGE, data);
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+const loadProduct = () => {
+    try {
+        const data = localStorage.getItem(STORAGE);
+        if (!data)
+            return [];
+        return JSON.parse(data);
+    }
+    catch (_a) {
+        return [];
+    }
+};
+const renderProduct = () => {
+    productList.innerHTML = "";
+    if (products.length === 0) {
+        const emptyRow = document.createElement("tr");
+        emptyRow.innerHTML = `
+    <div class="empty-state" id="emptyState">
+          <div class="empty-icon">
+            <i class="fa-solid fa-receipt"></i>
+          </div>
+          <h3>No products yet</h3>
+          <p>Add your first products.</p>
+        </div>`;
+        productList.appendChild(emptyRow);
+        saveProduct();
+    }
+    products.forEach((product) => {
+        const row = document.createElement("tr");
+        const formatAmount = product.amount.toLocaleString();
+        row.innerHTML = `
+    <td>${product.name}</td>
+    <td>${product.category}</td>
+    <td>${formatAmount}</td>
+    <td>${product.stock}</td>
+    <td>
+        <button class="action-button edit-button" data-id="${product.id}" aria-label="Edit transaction">
+             <i class="fa-solid fa-pen"></i>
+            </button>
+            <button class="action-button delete-button" data-id="${product.id}" aria-label="Delete transaction">
+              <i class="fa-solid fa-trash"></i>
+            </button>
+        </td>
+     `;
+        productList.appendChild(row);
+    });
+};
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = productName.value.trim();
+    const category = productCategory.value;
+    const amount = productAmount.valueAsNumber;
+    const quantity = productQuantity.valueAsNumber;
+    if (!name) {
+        alert("Please fill in the product name");
+    }
+    if (!category) {
+        alert("Please select the product's catgory");
+    }
+    if (!amount || !amount) {
+        alert("Please fill in the product's amount and quantity");
+    }
+    const product = {
+        id: Date.now(),
+        name: name,
+        category: category,
+        amount: amount,
+        stock: quantity,
+    };
+    products.push(product);
+    renderProduct();
+    saveProduct();
+});
+const init = () => {
+    loadProduct();
+    console.log("Data loaded successfully");
+};
+init();
 export {};
 //# sourceMappingURL=sandbox.js.map
--- Day-10-dashboard/src/sandbox.ts
+++ Day-10-dashboard/src/sandbox.ts
@@ -1,10 +1,128 @@
-const popup= document.querySelector('.add-modal-card')
-const openModal= document.querySelector('#addPopup')
-const closeModal= document.querySelector('#closePopup')
+const popup = document.querySelector(".add-modal-card");
+const openModal = document.querySelector("#addPopup") as HTMLButtonElement;
+const closeModal = document.querySelector("#closePopup") as HTMLButtonElement;
 
-openModal?.addEventListener('click', ()=>{
-  popup?.classList.add('show')
-})
-closeModal?.addEventListener('click', ()=>{
-  popup?.classList.remove('show')
-})
+openModal.addEventListener("click", () => {
+  popup?.classList.add("show");
+});
+closeModal.addEventListener("click", () => {
+  popup?.classList.remove("show");
+});
+const productName = document.querySelector("#productName") as HTMLInputElement;
+const productCategory = document.querySelector(
+  "#productCategory",
+) as HTMLSelectElement;
+const productAmount = document.querySelector(
+  "#productAmount",
+) as HTMLInputElement;
+const productQuantity = document.querySelector(
+  "#productQuantity",
+) as HTMLInputElement;
+const productList = document.querySelector(
+  "#productList",
+) as HTMLTableSectionElement;
+const submitBtn = document.querySelector(
+  "#addProductButton",
+) as HTMLButtonElement;
+const form = document.querySelector("#productForm") as HTMLFormElement;
+interface Product {
+  id: number;
+  name: string;
+  amount: number;
+  category: string;
+  stock: number;
+}
+const products: Product[] = [];
+const STORAGE = "products";
+
+const saveProduct = () => {
+  try {
+    const data = JSON.stringify(products);
+    localStorage.setItem(STORAGE, data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+const loadProduct = (): Product[] => {
+  try {
+    const data = localStorage.getItem(STORAGE);
+    if (!data) return [];
+    return JSON.parse(data) as Product[];
+  } catch {
+    return [];
+  }
+};
+
+const renderProduct = () => {
+  productList.innerHTML = "";
+  if (products.length === 0) {
+    const emptyRow = document.createElement("tr");
+    emptyRow.innerHTML = `
+    <div class="empty-state" id="emptyState">
+          <div class="empty-icon">
+            <i class="fa-solid fa-receipt"></i>
+          </div>
+          <h3>No products yet</h3>
+          <p>Add your first products.</p>
+        </div>`;
+    productList.appendChild(emptyRow);
+    saveProduct();
+  }
+
+  products.forEach((product) => {
+    const row = document.createElement("tr");
+    const formatAmount = product.amount.toLocaleString();
+    row.innerHTML = `
+    <td>${product.name}</td>
+    <td>${product.category}</td>
+    <td>${formatAmount}</td>
+    <td>${product.stock}</td>
+    <td>
+        <button class="action-button edit-button" data-id="${product.id}" aria-label="Edit transaction">
+             <i class="fa-solid fa-pen"></i>
+            </button>
+            <button class="action-button delete-button" data-id="${product.id}" aria-label="Delete transaction">
+              <i class="fa-solid fa-trash"></i>
+            </button>
+        </td>
+     `;
+       productList.appendChild(row)
+  });
+
+};
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  const name = productName.value.trim();
+  const category = productCategory.value;
+  const amount = productAmount.valueAsNumber;
+  const quantity = productQuantity.valueAsNumber;
+
+  if (!name) {
+    alert("Please fill in the product name");
+  }
+  if (!category) {
+    alert("Please select the product's catgory");
+  }
+  if (!amount || !amount) {
+    alert("Please fill in the product's amount and quantity");
+  }
+
+  const product: Product = {
+    id: Date.now(),
+    name: name,
+    category: category,
+    amount: amount,
+    stock: quantity,
+  };
+  products.push(product);
+  renderProduct();
+  saveProduct();
+});
+
+const init = () => {
+  loadProduct();
+  console.log("Data loaded successfully");
+};
+init();
